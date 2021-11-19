a83fe565c73e284f673d3308ee2b07c7546a29dc
Alisson Silva <alissonsoaressocial02@gmail.com>
2021-11-18 21:15:42 -0300

    Exercício terminado

--- Semana 4/Atividade/número primo.py
+++ Semana 4/Atividade/número primo.py
@@ -1,5 +1,17 @@
-i=1
 
-while i != 0: 
-    x = int(input("Escreva um número e 0 pra sair: "))
-            
+x = int(input("Escreva um numero: "))
+i=1
+res=0
+while i <= x:
+    if x%i==0:
+        res=res+1        
+    i=i+1
+if res==2 or res==1:
+    print("primo")
+else:
+    print("nao primo")
+
+print(res)
+
+
+
